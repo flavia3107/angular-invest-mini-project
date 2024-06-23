392bccaf5e1ba6a70b02b4ae154abd92bd8bf13a
Flavia Kosti <flaviakosti@gmail.com>
2024-06-23 18:56:47 -0400

    Invest: Add service

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,5 +1,7 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-result',
@@ -9,12 +11,9 @@ import { Component, input } from '@angular/core';
   styleUrl: './investment-result.component.css'
 })
 export class InvestmentResultComponent {
-  results = input<{
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[]>()
+  private investmentService = inject(InvestmentService);
+
+  get results() {
+    return this.investmentService.resultData;
+  }
 }
